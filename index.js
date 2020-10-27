aad61e177900a4506c0cedd7eebf101bda039ea9
actiontestscript <73528981+actiontestscript@users.noreply.github.com>
2020-10-27 12:21:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-module.exports = function install(string) {
 
-  return "";
-};
+  var http = require('http');
+var fs = require('fs');
+
+var file = fs.createWriteStream("file.wav");
+var request = http.get("http://static1.grsites.com/archive/sounds/comic/comic002.wav", function(response) {
+  response.pipe(file);
+});
